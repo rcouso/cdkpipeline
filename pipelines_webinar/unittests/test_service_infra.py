4b59582426b5c36b7d94d0bfe56c847f41d8f2b2
rubencouso <ruben.couso@gmail.com>
2020-11-16 02:34:45 +0100

    Adding unittest and requirements.txt pytest

--- pipelines_webinar/unittests/test_service_infra.py
+++ pipelines_webinar/unittests/test_service_infra.py
@@ -11,6 +11,6 @@ def test_lambda_handler():
     #THEN
     template = app.synth().get_stack_by_name("Stack").template
     functions =   [resource for resource in template["Resources"].values()
-                    if resource["Type"] == "AWS::Lambda::Functions"]
+                    if resource["Type"] == "AWS::Lambda::Function"]
     assert len(functions) == 1
     assert functions[0]["Properties"]["Handler"] == "handler.handler"
