fadf31bcd8e2767439224e5156c4f543b7cc4976
rubencouso <ruben.couso@gmail.com>
2020-11-16 04:38:15 +0100

    Deployment group

--- pipelines_webinar/pipelines_webinar_stack.py
+++ pipelines_webinar/pipelines_webinar_stack.py
@@ -4,7 +4,7 @@ from aws_cdk import core
 
 import aws_cdk.aws_lambda as lmb
 import aws_cdk.aws_apigateway as apigw
-
+import aws_cdk.aws_codedeploy as codedeploy
 
 class PipelinesWebinarStack(core.Stack):
 
@@ -18,7 +18,13 @@ class PipelinesWebinarStack(core.Stack):
             runtime=lmb.Runtime.PYTHON_3_7,
             handler='handler.handler',
             code=lmb.Code.from_asset(path.join(this_dir, 'lambda')))
+        alias = lmb.Alias(self, "HandlerAlias",
+            alias_name="Current",
+            version=handler.current_version)
         gw = apigw.LambdaRestApi(self, 'Gateway',
             description='Endpoint for a singple Lambda-powered web service',
-            handler=handler.current_version)
+            handler=alias)
+        codedeploy.LambdaDeploymentGroup(self,
+            alias=alias,
+            deployment_config=codedeploy.LambdaDeploymentConfig.CANARY_10_PERCENT_10_MINUTES)
         self.url_output = core.CfnOutput(self, 'Url', value=gw.url)
