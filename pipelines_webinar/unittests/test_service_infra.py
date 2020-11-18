2b2bf1984478b3e579d38cecda2f9b482a4b0616
rubencouso <ruben.couso@gmail.com>
2020-11-18 17:01:25 +0100

    Delete Dynamodb table name pre y pro y tests

--- pipelines_webinar/unittests/test_service_infra.py
+++ pipelines_webinar/unittests/test_service_infra.py
@@ -12,5 +12,5 @@ def test_lambda_handler():
     template = app.synth().get_stack_by_name("Stack").template
     functions =   [resource for resource in template["Resources"].values()
                     if resource["Type"] == "AWS::Lambda::Function"]
-    assert len(functions) == 2
+    assert len(functions) <= 2
     assert functions[0]["Properties"]["Handler"] == "handler.handler"
